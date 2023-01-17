89e634e0f5174ea6a3d01dc27754d73e25343a7f
kema-ray <rachelkemuma99@gmail.com>
2023-01-17 14:41:20 +0300

    create endpoints for advocates

--- cados/views.py
+++ cados/views.py
@@ -5,3 +5,11 @@ from django.http import JsonResponse
 def endpoints(request):
         data = ['/advocates/', 'advocates/:username']
         return JsonResponse(data, safe=False)
+
+def advocate_list(request):
+        data = ['Dennis','Rachel','Betty']
+        return JsonResponse(data, safe=False)
+
+def advocate_detail(request, username):
+        data = username
+        return JsonResponse(data, safe=False)
