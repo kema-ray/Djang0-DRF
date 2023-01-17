ad45a69bb7e5a3236b747d3de01c0a6353b6f444
kema-ray <rachelkemuma99@gmail.com>
2023-01-17 14:22:39 +0300

    create simple endpoints view and urls

--- cados/views.py
+++ cados/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def endpoints(request):
+        data = ['/advocates/', 'advocates/:username']
+        return JsonResponse(data, safe=False)
