58a53e4c5e6bb3404f4143d52349d4c261f48b29
kema-ray <rachelkemuma99@gmail.com>
2023-01-18 14:03:53 +0300

    Decorator that converts a function-based view into an APIView subclass

--- cados/views.py
+++ cados/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
 def endpoints(request):
         data = ['/advocates/', 'advocates/:username']
-        return JsonResponse(data, safe=False)
+        return Response(data)
+        # return JsonResponse(data, safe=False)
 
+@api_view(['GET'])
 def advocate_list(request):
         data = ['Dennis','Rachel','Betty']
-        return JsonResponse(data, safe=False)
+        return Response(data)
+        # return JsonResponse(data, safe=False)
 
+@api_view(['GET'])
 def advocate_detail(request, username):
         data = username
-        return JsonResponse(data, safe=False)
+        return Response(data)
+        # return JsonResponse(data, safe=False)
