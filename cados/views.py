da58ed88946341c2ed8862772cac61b8b2429ae4
kema-ray <rachelkemuma99@gmail.com>
2023-01-20 10:30:00 +0300

    add a post method to add a new user

--- cados/views.py
+++ cados/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,7 +13,7 @@ def endpoints(request):
         return Response(data)
         # return JsonResponse(data, safe=False)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def advocate_list(request):
         # Handles GET requests
         if request.method == 'GET':
@@ -26,6 +26,14 @@ def advocate_list(request):
                 serializer = AdvocateSerializer(advocates, many=True)
                 return Response(serializer.data)
         # return JsonResponse(data, safe=False)
+        if request.method == 'POST':
+                advocate = Advocate.objects.create(
+                        username = request.data['username'],
+                        bio = request.data['bio']
+                )
+                serializer = AdvocateSerializer(advocate, many = False)
+
+                return Response(serializer.data)
 
 @api_view(['GET'])
 def advocate_detail(request, username):
