51062d3236c647580fe9ac09c5f54a2b4ae90d3f
kema-ray <rachelkemuma99@gmail.com>
2023-01-20 15:15:05 +0300

    Create a one to many relationship between advocate and company

--- cados/models.py
+++ cados/models.py
@@ -8,6 +8,7 @@ class Company(models.Model):
         def __str__(self):
                 return self.name
 class Advocate(models.Model):
+        company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
         username = models.CharField(max_length=200)
         bio = models.TextField(max_length=250, null=True,blank=True)
 
