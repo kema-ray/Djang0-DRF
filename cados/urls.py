787e6fc9022976b522003c6633e057366dd3e431
kema-ray <rachelkemuma99@gmail.com>
2023-01-24 15:18:51 +0300

    add  routes for Simple JWT

--- cados/urls.py
+++ cados/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+#     TokenRefreshView,
+)
+
 urlpatterns = [
         path('', views.endpoints),
+        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
         path('advocates/', views.advocate_list, name="adcocates"),
         # path('advocates/<str:username>/', views.advocate_detail),
         
